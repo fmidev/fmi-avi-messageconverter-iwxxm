dae315e477d134d3a08eba494236527e1282267a
Ilkka Rinne <ilkka.rinne@spatineo.com>
2018-08-28 16:57:26 +0300

    Added a few JUnit tests for METAR parsing, and a TODO list for additional tests to improve test coverage

--- src/test/java/fi/fmi/avi/converter/iwxxm/METARIWXXMParserTest.java
+++ src/test/java/fi/fmi/avi/converter/iwxxm/METARIWXXMParserTest.java
@@ -1,10 +1,11 @@
 package fi.fmi.avi.converter.iwxxm;
 
-import fi.fmi.avi.converter.AviMessageConverter;
-import fi.fmi.avi.converter.ConversionHints;
-import fi.fmi.avi.converter.ConversionResult;
-import fi.fmi.avi.converter.iwxxm.conf.IWXXMConverter;
-import fi.fmi.avi.model.metar.METAR;
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.InputStream;
+import java.util.Objects;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +15,11 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.unitils.thirdparty.org.apache.commons.io.IOUtils;
 import org.w3c.dom.Document;
 
-import java.io.InputStream;
-import java.util.Objects;
-
-import static org.junit.Assert.assertTrue;
+import fi.fmi.avi.converter.AviMessageConverter;
+import fi.fmi.avi.converter.ConversionHints;
+import fi.fmi.avi.converter.ConversionResult;
+import fi.fmi.avi.converter.iwxxm.conf.IWXXMConverter;
+import fi.fmi.avi.model.metar.METAR;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = IWXXMTestConfiguration.class, loader = AnnotationConfigContextLoader.class)
@@ -56,4 +58,54 @@ public class METARIWXXMParserTest extends DOMParsingTestBase {
         ConversionResult<METAR> result = converter.convertMessage(toValidate, IWXXMConverter.IWXXM21_DOM_TO_METAR_POJO, ConversionHints.EMPTY);
         assertTrue( "No issues should have been found", result.getConversionIssues().isEmpty());
     }
+
+    @Test
+    public void testNOSIG() throws Exception {
+        Document toValidate = readDocument("metar-A3-1_NOSIG.xml");
+        ConversionResult<METAR> result = converter.convertMessage(toValidate, IWXXMConverter.IWXXM21_DOM_TO_METAR_POJO, ConversionHints.EMPTY);
+        assertTrue("No issues should have been found", result.getConversionIssues().isEmpty());
+    }
+
+    @Test
+    public void testCatchesWrongObservationTypes() throws Exception {
+        Document toValidate = readDocument("metar-A3-1_invalid-obs-types.xml");
+        ConversionResult<METAR> result = converter.convertMessage(toValidate, IWXXMConverter.IWXXM21_DOM_TO_METAR_POJO, ConversionHints.EMPTY);
+        assertFalse("Issues should have been found", result.getConversionIssues().isEmpty());
+        assertTrue(result.getConversionIssues().stream().filter(issue -> issue.getMessage().contains("Invalid observation type")).count() == 2);
+    }
+
+    @Test
+    public void testCatchesWrongObservedPropertyRefs() throws Exception {
+        Document toValidate = readDocument("metar-A3-1_invalid-obs-properties.xml");
+        ConversionResult<METAR> result = converter.convertMessage(toValidate, IWXXMConverter.IWXXM21_DOM_TO_METAR_POJO, ConversionHints.EMPTY);
+        assertFalse("Issues should have been found", result.getConversionIssues().isEmpty());
+        assertTrue(result.getConversionIssues().stream().filter(issue -> issue.getMessage().contains("Invalid observed property")).count() == 2);
+    }
+
+    @Test
+    public void testCatchesMissingPhenomenonTime() throws Exception {
+        Document toValidate = readDocument("metar-A3-1_no-phenomenon-time.xml");
+        ConversionResult<METAR> result = converter.convertMessage(toValidate, IWXXMConverter.IWXXM21_DOM_TO_METAR_POJO, ConversionHints.EMPTY);
+        assertFalse("Issues should have been found", result.getConversionIssues().isEmpty());
+        assertTrue(result.getConversionIssues().stream().filter(issue -> issue.getMessage().contains("METAR observation phenomenonTime")).count() == 1);
+    }
+
+    //TODO: recent weather
+    //TODO: wind shear
+    //TODO: sea state
+    //TODO: snow closure (RWS)
+    //TODO: CAVOK with conflicting data
+    //TODO: trend wind
+    //TODO: trend CAVOK with conflicts
+    //TODO: trend cloud (with NSC)
+    //TODO: RWS with snow closure conflicts
+    //TODO: RWS with all runways flag
+    //TODO: RWS with cleared flag conflicts
+    //TODO: RWS with depth of deposit
+    //TODO: RWS with braking action special values 99 and 127
+    //TODO: observed cloud with nilReasons
+    //TODO: trend cloud
+
+
+
 }
