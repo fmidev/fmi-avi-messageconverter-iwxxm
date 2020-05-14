8180f2db00c72f92dd0568ce4a300098b81ff6ec
Ilkka Rinne <ilkka.rinne@spatineo.com>
2020-05-14 14:07:20 +0300

    Added generics to the class declaration

--- src/main/java/fi/fmi/avi/converter/iwxxm/v30/AbstractIWXXM30Serializer.java
+++ src/main/java/fi/fmi/avi/converter/iwxxm/v30/AbstractIWXXM30Serializer.java
@@ -1,63 +1,11 @@
 package fi.fmi.avi.converter.iwxxm.v30;
 
-import java.math.BigInteger;
-import java.time.ZonedDateTime;
-import java.util.Optional;
-import java.util.UUID;
-
-import javax.xml.bind.JAXBElement;
-import javax.xml.namespace.QName;
-
-import net.opengis.gml32.AbstractGeometryType;
-import net.opengis.gml32.AbstractTimeObjectType;
-import net.opengis.gml32.DirectPositionType;
-import net.opengis.gml32.FeaturePropertyType;
-import net.opengis.gml32.PointType;
-import net.opengis.gml32.ReferenceType;
-import net.opengis.gml32.TimeInstantType;
-import net.opengis.gml32.TimePeriodType;
-import net.opengis.gml32.TimePrimitivePropertyType;
-/*
-import net.opengis.om20.OMObservationType;
-import net.opengis.om20.TimeObjectPropertyType;
-import net.opengis.sampling.spatial.SFSpatialSamplingFeatureType;
-import net.opengis.sampling.spatial.ShapeType;
-*/
-import aero.aixm511.AirportHeliportTimeSlicePropertyType;
-import aero.aixm511.AirportHeliportTimeSliceType;
-import aero.aixm511.AirportHeliportType;
-import aero.aixm511.CodeAirportHeliportDesignatorType;
-import aero.aixm511.CodeIATAType;
-import aero.aixm511.CodeICAOType;
-import aero.aixm511.ElevatedPointPropertyType;
-import aero.aixm511.ElevatedPointType;
-import aero.aixm511.TextNameType;
-import aero.aixm511.ValDistanceVerticalType;
-import fi.fmi.avi.converter.ConversionResult;
 import fi.fmi.avi.converter.iwxxm.AbstractIWXXMSerializer;
-import fi.fmi.avi.converter.iwxxm.IWXXMNamespaceContext;
-import fi.fmi.avi.converter.iwxxm.ReferredObjectRetrievalContext;
 import fi.fmi.avi.converter.iwxxm.XMLSchemaInfo;
-import fi.fmi.avi.model.Aerodrome;
-import fi.fmi.avi.model.AviationCodeListUser;
-import fi.fmi.avi.model.CloudForecast;
-import fi.fmi.avi.model.CloudLayer;
-import fi.fmi.avi.model.GeoPosition;
-import fi.fmi.avi.model.NumericMeasure;
-import fi.fmi.avi.model.PartialOrCompleteTimeInstant;
-import fi.fmi.avi.model.PartialOrCompleteTimePeriod;
-/*
-import icao.iwxxm21.AerodromeCloudForecastType;
-import icao.iwxxm21.CloudAmountReportedAtAerodromeType;
-import icao.iwxxm21.CloudLayerType;
-import icao.iwxxm21.DistanceWithNilReasonType;
-import icao.iwxxm21.LengthWithNilReasonType;
-import icao.iwxxm21.SigConvectiveCloudTypeType;
-
- */
+import fi.fmi.avi.model.AviationWeatherMessageOrCollection;
 import icao.iwxxm30.SpaceWeatherAdvisoryType;
 
-public abstract class AbstractIWXXM30Serializer extends AbstractIWXXMSerializer {
+public abstract class AbstractIWXXM30Serializer<T extends AviationWeatherMessageOrCollection, S> extends AbstractIWXXMSerializer<T, S> {
 
     @Override
     protected XMLSchemaInfo getSchemaInfo() {
