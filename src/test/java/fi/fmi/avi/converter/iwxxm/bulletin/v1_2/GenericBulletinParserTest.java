7bdad7b89a2e06a18e6c4b0e0d1b2da63a5cc9d4
patrick-alaspaa <patrick.alaspaa@spatineo.com>
2021-06-03 09:58:51 +0300

    ISSUE #80
    Add bulletin parser tests and test resources for IWXXM 3.0 regular and cancellation message parsing.

--- src/test/java/fi/fmi/avi/converter/iwxxm/bulletin/v1_2/GenericBulletinParserTest.java
+++ src/test/java/fi/fmi/avi/converter/iwxxm/bulletin/v1_2/GenericBulletinParserTest.java
@@ -22,6 +22,9 @@ import fi.fmi.avi.converter.ConversionHints;
 import fi.fmi.avi.converter.ConversionResult;
 import fi.fmi.avi.converter.iwxxm.IWXXMTestConfiguration;
 import fi.fmi.avi.converter.iwxxm.conf.IWXXMConverter;
+import fi.fmi.avi.model.Aerodrome;
+import fi.fmi.avi.model.AviationWeatherMessage;
+import fi.fmi.avi.model.GenericAviationWeatherMessage;
 import fi.fmi.avi.model.bulletin.GenericMeteorologicalBulletin;
 
 /**
@@ -67,5 +70,47 @@ public class GenericBulletinParserTest {
         final ConversionResult<GenericMeteorologicalBulletin> result = this.converter.convertMessage(input, IWXXMConverter.IWXXM21_DOM_TO_GENERIC_BULLETIN_POJO,
                 ConversionHints.EMPTY);
         assertEquals(ConversionResult.Status.SUCCESS, result.getStatus());
+    }
+
+    @Test
+    public void testParserWithIWXXM30() throws Exception {
+        final Document input = this.getBulletinDocument("iwxxm-30-bulletin.xml");
+        final ConversionResult<GenericMeteorologicalBulletin> result = this.converter.convertMessage(input, IWXXMConverter.IWXXM30_DOM_TO_GENERIC_BULLETIN_POJO,
+                ConversionHints.EMPTY);
+        assertEquals(ConversionResult.Status.SUCCESS, result.getStatus());
+
+        GenericAviationWeatherMessage message = result.getConvertedMessage().get().getMessages().get(0);
+
+        //check validtime
+        assertEquals("2012-08-16T00:00Z", message.getValidityTime().get().getStartTime().get().getCompleteTime().get().toString());
+        assertEquals("2012-08-16T18:00Z", message.getValidityTime().get().getEndTime().get().getCompleteTime().get().toString());
+
+        //check aerodrome
+        Aerodrome aerodrome = message.getTargetAerodrome().get();
+        assertEquals("YUDO", aerodrome.getDesignator());
+        assertEquals("DONLON/INTERNATIONAL", aerodrome.getName().get());
+        assertEquals("YUDO", aerodrome.getLocationIndicatorICAO().get());
+        assertEquals("DON", aerodrome.getDesignatorIATA().get());
+        //check status
+        assertEquals(AviationWeatherMessage.ReportStatus.NORMAL, message.getReportStatus());
+    }
+
+    @Test
+    public void testParserWithIWXXM30Cancellation() throws Exception {
+        final Document input = this.getBulletinDocument("iwxxm-30-cancellation-bulletin.xml");
+        final ConversionResult<GenericMeteorologicalBulletin> result = this.converter.convertMessage(input, IWXXMConverter.IWXXM30_DOM_TO_GENERIC_BULLETIN_POJO,
+                ConversionHints.EMPTY);
+        assertEquals(ConversionResult.Status.SUCCESS, result.getStatus());
+
+        GenericAviationWeatherMessage message = result.getConvertedMessage().get().getMessages().get(0);
+        assertEquals("2012-08-16T00:00Z", message.getValidityTime().get().getStartTime().get().getCompleteTime().get().toString());
+        assertEquals("2012-08-16T18:00Z", message.getValidityTime().get().getEndTime().get().getCompleteTime().get().toString());
+
+        Aerodrome aerodrome = message.getTargetAerodrome().get();
+        assertEquals("YUDO", aerodrome.getDesignator());
+        assertEquals("DONLON/INTERNATIONAL", aerodrome.getName().get());
+        assertEquals("YUDO", aerodrome.getLocationIndicatorICAO().get());
+
+        assertEquals(AviationWeatherMessage.ReportStatus.AMENDMENT, message.getReportStatus());
     }
 }
