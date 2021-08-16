ebaf89706922c40f34ef550284edf06d9576b500
patrick-alaspaa <patrick.alaspaa@spatineo.com>
2021-08-16 10:38:09 +0300

    ISSUE #88 Configuration refactor

--- src/main/java/fi/fmi/avi/converter/iwxxm/conf/IWXXMGenericAviationWeatherMessageConverter.java
+++ src/main/java/fi/fmi/avi/converter/iwxxm/conf/IWXXMGenericAviationWeatherMessageConverter.java
@@ -1,5 +1,8 @@
 package fi.fmi.avi.converter.iwxxm.conf;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +13,8 @@ import fi.fmi.avi.converter.iwxxm.generic.GenericAviationWeatherMessageParser;
 import fi.fmi.avi.converter.iwxxm.generic.GenericAviationWeatherMessageScanner;
 import fi.fmi.avi.converter.iwxxm.generic.GenericBulletinIWXXMParser;
 import fi.fmi.avi.converter.iwxxm.generic.IWXXMGenericBulletinScanner;
+import fi.fmi.avi.converter.iwxxm.v2_1.sigmet.GenericSIGMETIWXXMScanner;
+import fi.fmi.avi.converter.iwxxm.v2_1.taf.GenericTAFIWXXMScanner;
 import fi.fmi.avi.model.GenericAviationWeatherMessage;
 import fi.fmi.avi.model.bulletin.GenericMeteorologicalBulletin;
 
@@ -17,8 +22,47 @@ import fi.fmi.avi.model.bulletin.GenericMeteorologicalBulletin;
 public class IWXXMGenericAviationWeatherMessageConverter {
 
     @Bean
+    public GenericSIGMETIWXXMScanner genericSIGMETIWXXM21Scanner() {
+        return new GenericSIGMETIWXXMScanner();
+    }
+
+    @Bean
+    public fi.fmi.avi.converter.iwxxm.v3_0.sigmet.GenericSIGMETIWXXMScanner genericSIGMETIWXXM30Scanner() {
+        return new fi.fmi.avi.converter.iwxxm.v3_0.sigmet.GenericSIGMETIWXXMScanner();
+    }
+
+    @Bean
+    public GenericTAFIWXXMScanner genericTAFIWXXM21Scanner() {
+        return new GenericTAFIWXXMScanner();
+    }
+
+    @Bean
+    public fi.fmi.avi.converter.iwxxm.v3_0.taf.GenericTAFIWXXMScanner genericTAFIWXXM30Scanner() {
+        return new fi.fmi.avi.converter.iwxxm.v3_0.taf.GenericTAFIWXXMScanner();
+    }
+
+    @Bean
     public GenericAviationWeatherMessageScanner genericAviationWeatherMessageIWXXMScanner() {
         return new GenericAviationWeatherMessageScanner();
+    }
+
+    @Bean
+    public CollectionBean collectionBean(GenericSIGMETIWXXMScanner genericSIGMETIWXXM21Scanner, GenericTAFIWXXMScanner genericTAFIWXXM21Scanner,
+            fi.fmi.avi.converter.iwxxm.v3_0.sigmet.GenericSIGMETIWXXMScanner genericSIGMETIWXXM30Scanner,
+            fi.fmi.avi.converter.iwxxm.v3_0.taf.GenericTAFIWXXMScanner genericTAFIWXXM30Scanner) {
+        Map<GenericAviationWeatherMessageParser.ScannerKey, fi.fmi.avi.converter.iwxxm.GenericAviationWeatherMessageScanner> genericMessageScannerMap =
+                new HashMap<>();
+
+        genericMessageScannerMap.put(new GenericAviationWeatherMessageParser.ScannerKey("http://icao.int/iwxxm/2.1", "TropicalCycloneSIGMET"), genericSIGMETIWXXM21Scanner);
+        genericMessageScannerMap.put(new GenericAviationWeatherMessageParser.ScannerKey("http://icao.int/iwxxm/3.0", "TropicalCycloneSIGMET"), genericSIGMETIWXXM30Scanner);
+        genericMessageScannerMap.put(new GenericAviationWeatherMessageParser.ScannerKey("http://icao.int/iwxxm/2.1", "VolcanicAshSIGMET"), genericSIGMETIWXXM21Scanner);
+        genericMessageScannerMap.put(new GenericAviationWeatherMessageParser.ScannerKey("http://icao.int/iwxxm/3.0", "VolcanicAshSIGMET"), genericSIGMETIWXXM30Scanner);
+        genericMessageScannerMap.put(new GenericAviationWeatherMessageParser.ScannerKey("http://icao.int/iwxxm/2.1", "SIGMET"), genericSIGMETIWXXM21Scanner);
+        genericMessageScannerMap.put(new GenericAviationWeatherMessageParser.ScannerKey("http://icao.int/iwxxm/3.0", "SIGMET"), genericSIGMETIWXXM30Scanner);
+        genericMessageScannerMap.put(new GenericAviationWeatherMessageParser.ScannerKey("http://icao.int/iwxxm/2.1", "TAF"), genericTAFIWXXM21Scanner);
+        genericMessageScannerMap.put(new GenericAviationWeatherMessageParser.ScannerKey("http://icao.int/iwxxm/3.0", "TAF"), genericTAFIWXXM30Scanner);
+
+        return new CollectionBean(genericMessageScannerMap);
     }
 
     @Bean
@@ -39,14 +83,24 @@ public class IWXXMGenericAviationWeatherMessageConverter {
 
     @Bean
     @Qualifier("genericAviationWeatherMessageIWXXMDOMParser")
-    public AviMessageSpecificConverter<Document, GenericAviationWeatherMessage> genericAviationWeatherMessageIWXXMDOMParser(GenericAviationWeatherMessageScanner genericAviationWeatherMessageIWXXMScanner) {
-        return new GenericAviationWeatherMessageParser.FromDOM(genericAviationWeatherMessageIWXXMScanner);
+    public AviMessageSpecificConverter<Document, GenericAviationWeatherMessage> genericAviationWeatherMessageIWXXMDOMParser(CollectionBean collectionBean) {
+        return new GenericAviationWeatherMessageParser.FromDOM(collectionBean.getMap());
     }
 
     @Bean
     @Qualifier("genericAviationWeatherMessageIWXXMStringParser")
-    public AviMessageSpecificConverter<String, GenericAviationWeatherMessage> genericAviationWeatherMessageIWXXMStringParser(GenericAviationWeatherMessageScanner genericAviationWeatherMessageIWXXMScanner) {
-        return new GenericAviationWeatherMessageParser.FromString(genericAviationWeatherMessageIWXXMScanner);
+    public AviMessageSpecificConverter<String, GenericAviationWeatherMessage> genericAviationWeatherMessageIWXXMStringParser(CollectionBean collectionBean) {
+        return new GenericAviationWeatherMessageParser.FromString(collectionBean.getMap());
+    }
+
+    private class CollectionBean {
+        private Map<GenericAviationWeatherMessageParser.ScannerKey, fi.fmi.avi.converter.iwxxm.GenericAviationWeatherMessageScanner> map;
+        public CollectionBean(Map map) {
+            this.map = map;
+        }
+        public Map<GenericAviationWeatherMessageParser.ScannerKey, fi.fmi.avi.converter.iwxxm.GenericAviationWeatherMessageScanner> getMap() {
+            return map;
+        }
     }
 
     // Serializers:
