10992306b3ae1117d9f59437420ea351e3e4f938
patrick-alaspaa <patrick.alaspaa@spatineo.com>
2021-08-20 10:17:20 +0300

    CR fix. Change bean types to be explicitly of type GenericAviationWeatherMessageParser in order to remove type casting and remove now redundant qualifiers

--- src/main/java/fi/fmi/avi/converter/iwxxm/conf/IWXXMGenericAviationWeatherMessageConverter.java
+++ src/main/java/fi/fmi/avi/converter/iwxxm/conf/IWXXMGenericAviationWeatherMessageConverter.java
@@ -1,29 +1,21 @@
 package fi.fmi.avi.converter.iwxxm.conf;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.w3c.dom.Document;
 
 import fi.fmi.avi.converter.AviMessageSpecificConverter;
-import fi.fmi.avi.converter.iwxxm.GenericAviationWeatherMessageScanner;
 import fi.fmi.avi.converter.iwxxm.generic.GenericAviationWeatherMessageParser;
 import fi.fmi.avi.converter.iwxxm.generic.GenericBulletinIWXXMParser;
 import fi.fmi.avi.converter.iwxxm.generic.IWXXMGenericBulletinScanner;
-import fi.fmi.avi.converter.iwxxm.v2_1.sigmet.GenericSIGMETIWXXMScanner;
-import fi.fmi.avi.converter.iwxxm.v2_1.taf.GenericTAFIWXXMScanner;
-import fi.fmi.avi.model.GenericAviationWeatherMessage;
 import fi.fmi.avi.model.bulletin.GenericMeteorologicalBulletin;
 
 @Configuration
 public class IWXXMGenericAviationWeatherMessageConverter {
 
     @Bean
-    public IWXXMGenericBulletinScanner iwxxmGenericBulletinScanner(@Qualifier("genericAviationWeatherMessageIWXXMDOMParser") AviMessageSpecificConverter<Document, GenericAviationWeatherMessage> messageParser) {
-        return new IWXXMGenericBulletinScanner((GenericAviationWeatherMessageParser) messageParser);
+    public IWXXMGenericBulletinScanner iwxxmGenericBulletinScanner(final GenericAviationWeatherMessageParser<Document> messageParser) {
+        return new IWXXMGenericBulletinScanner(messageParser);
     }
 
     // Parsers:
@@ -38,14 +30,12 @@ public class IWXXMGenericAviationWeatherMessageConverter {
     }
 
     @Bean
-    @Qualifier("genericAviationWeatherMessageIWXXMDOMParser")
-    public AviMessageSpecificConverter<Document, GenericAviationWeatherMessage> genericAviationWeatherMessageIWXXMDOMParser() {
+    public GenericAviationWeatherMessageParser<Document> genericAviationWeatherMessageIWXXMDOMParser() {
         return new GenericAviationWeatherMessageParser.FromDOM();
     }
 
     @Bean
-    @Qualifier("genericAviationWeatherMessageIWXXMStringParser")
-    public AviMessageSpecificConverter<String, GenericAviationWeatherMessage> genericAviationWeatherMessageIWXXMStringParser() {
+    public GenericAviationWeatherMessageParser<String> genericAviationWeatherMessageIWXXMStringParser() {
         return new GenericAviationWeatherMessageParser.FromString();
     }
 
