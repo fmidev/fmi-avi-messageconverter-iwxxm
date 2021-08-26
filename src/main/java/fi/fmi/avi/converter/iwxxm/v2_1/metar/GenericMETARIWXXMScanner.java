5391479ff841fea317ea6d737d9ec89010935d1f
patrick-alaspaa <patrick.alaspaa@spatineo.com>
2021-08-26 12:13:24 +0300

    ISSUE #94 Add test xml files and new generic scanner classes

---
+++ src/main/java/fi/fmi/avi/converter/iwxxm/v2_1/metar/GenericMETARIWXXMScanner.java
@@ -0,0 +1,2 @@
+package fi.fmi.avi.converter.iwxxm.v2_1.metar;public class GenericMETARIWXXMScanner {
+}
---
+++ src/main/java/fi/fmi/avi/converter/iwxxm/v3_0/metar/GenericMETARIWXXMScanner.java
@@ -0,0 +1,2 @@
+package fi.fmi.avi.converter.iwxxm.v3_0.metar;public class GenericMETARIWXXMScanner {
+}
