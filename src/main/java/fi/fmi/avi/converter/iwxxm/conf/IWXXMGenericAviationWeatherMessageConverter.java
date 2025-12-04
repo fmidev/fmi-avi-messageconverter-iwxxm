5d9f94e506541a190c86ad0ce7b3934628d3fe79
kasarit <49980037+kasarit@users.noreply.github.com>
2025-12-04 12:29:02 +0200

    SDM-2628: Trying out a more declarative approach

--- src/main/java/fi/fmi/avi/converter/iwxxm/conf/IWXXMGenericAviationWeatherMessageConverter.java
+++ src/main/java/fi/fmi/avi/converter/iwxxm/conf/IWXXMGenericAviationWeatherMessageConverter.java
@@ -3,18 +3,14 @@ package fi.fmi.avi.converter.iwxxm.conf;
 import fi.fmi.avi.converter.AviMessageSpecificConverter;
 import fi.fmi.avi.converter.iwxxm.generic.*;
 import fi.fmi.avi.converter.iwxxm.generic.GenericAviationWeatherMessageParser.ScannerKey;
-import fi.fmi.avi.converter.iwxxm.generic.metar.GenericMETARSPECIIWXXMScanner;
 import fi.fmi.avi.converter.iwxxm.generic.metar.METARSPECIFieldXPathProvider;
-import fi.fmi.avi.converter.iwxxm.generic.sigmet.GenericSIGMETAIRMETIWXXMScanner;
 import fi.fmi.avi.converter.iwxxm.generic.sigmet.SIGMETAIRMETFieldXPathProvider;
-import fi.fmi.avi.converter.iwxxm.generic.swx.GenericSWXIWXXMScanner;
 import fi.fmi.avi.converter.iwxxm.generic.swx.SWXFieldXPathProvider;
-import fi.fmi.avi.converter.iwxxm.generic.taf.GenericTAFIWXXMScanner;
 import fi.fmi.avi.converter.iwxxm.generic.taf.TAFFieldXPathProvider;
-import fi.fmi.avi.converter.iwxxm.generic.tca.GenericTCAIWXXMScanner;
 import fi.fmi.avi.converter.iwxxm.generic.tca.TCAFieldXPathProvider;
-import fi.fmi.avi.converter.iwxxm.generic.vaa.GenericVAAIWXXMScanner;
 import fi.fmi.avi.converter.iwxxm.generic.vaa.VAAFieldXPathProvider;
+import fi.fmi.avi.model.GenericAviationWeatherMessage.LocationIndicatorType;
+import fi.fmi.avi.model.MessageType;
 import fi.fmi.avi.model.bulletin.GenericMeteorologicalBulletin;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -24,9 +20,47 @@ import org.w3c.dom.Element;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Configuration
 public class IWXXMGenericAviationWeatherMessageConverter {
+
+    /**
+     * Resolves METAR or SPECI message type from element local name.
+     * <p>
+     * Note: old Spring version, can't use lambdas.
+     * </p>
+     */
+    private static final Function<Element, MessageType> METAR_SPECI_TYPE_RESOLVER = new Function<Element, MessageType>() {
+        @Override
+        public MessageType apply(final Element element) {
+            final String name = element.getLocalName();
+            if ("METAR".equals(name)) {
+                return MessageType.METAR;
+            }
+            if ("SPECI".equals(name)) {
+                return MessageType.SPECI;
+            }
+            return null;
+        }
+    };
+
+    /**
+     * Resolves SIGMET or AIRMET message type from element local name.
+     * <p>
+     * Note: old Spring version, can't use lambdas.
+     * </p>
+     */
+    private static final Function<Element, MessageType> SIGMET_AIRMET_TYPE_RESOLVER = new Function<Element, MessageType>() {
+        @Override
+        public MessageType apply(final Element element) {
+            final String name = element.getLocalName();
+            if ("AIRMET".equals(name)) {
+                return MessageType.AIRMET;
+            }
+            return MessageType.SIGMET; // SIGMET, VolcanicAshSIGMET, TropicalCycloneSIGMET
+        }
+    };
 
     @Bean
     public IWXXMGenericBulletinScanner iwxxmGenericBulletinScanner(
@@ -71,35 +105,71 @@ public class IWXXMGenericAviationWeatherMessageConverter {
     public Map<ScannerKey, GenericAviationWeatherMessageScanner> genericAviationMessageScannerMap() {
         final Map<ScannerKey, GenericAviationWeatherMessageScanner> scannersMap = new HashMap<>();
 
-        // XPath field providers
-        final FieldXPathProvider tafFieldProvider = new TAFFieldXPathProvider();
-        final FieldXPathProvider sigmetAirmetFieldProvider = new SIGMETAIRMETFieldXPathProvider();
-        final FieldXPathProvider vaaFieldProvider = new VAAFieldXPathProvider();
-        final FieldXPathProvider tcaFieldProvider = new TCAFieldXPathProvider();
-        final FieldXPathProvider metarFieldProvider = new METARSPECIFieldXPathProvider();
-        final FieldXPathProvider swxFieldProvider = new SWXFieldXPathProvider();
+        // TAF scanner
+        final GenericIWXXMScanner tafScanner = GenericIWXXMScanner.builder()
+                .fieldProvider(new TAFFieldXPathProvider())
+                .messageType(MessageType.TAF)
+                .requireReportStatus(true)
+                .extractValidityTime(true)
+                .locationIndicator(LocationIndicatorType.AERODROME, IWXXMField.AERODROME)
+                .build();
+        scannersMap.put(new ScannerKey("TAF"), tafScanner);
 
-        final GenericTAFIWXXMScanner genericTafScanner = new GenericTAFIWXXMScanner(tafFieldProvider);
-        scannersMap.put(new ScannerKey("TAF"), genericTafScanner);
+        // METAR/SPECI scanner
+        final GenericIWXXMScanner metarSpeciScanner = GenericIWXXMScanner.builder()
+                .fieldProvider(new METARSPECIFieldXPathProvider())
+                .messageTypeResolver(METAR_SPECI_TYPE_RESOLVER)
+                .requireReportStatus(true)
+                .extractValidityTime(false)
+                .locationIndicator(LocationIndicatorType.AERODROME, IWXXMField.AERODROME)
+                .build();
+        scannersMap.put(new ScannerKey("METAR"), metarSpeciScanner);
+        scannersMap.put(new ScannerKey("SPECI"), metarSpeciScanner);
 
-        final GenericSWXIWXXMScanner genericSpaceWeatherScanner = new GenericSWXIWXXMScanner(swxFieldProvider);
-        scannersMap.put(new ScannerKey("SpaceWeatherAdvisory"), genericSpaceWeatherScanner);
+        // SIGMET/AIRMET scanner
+        final GenericIWXXMScanner sigmetAirmetScanner = GenericIWXXMScanner.builder()
+                .fieldProvider(new SIGMETAIRMETFieldXPathProvider())
+                .messageTypeResolver(SIGMET_AIRMET_TYPE_RESOLVER)
+                .requireReportStatus(true)
+                .extractValidityTime(true)
+                .locationIndicator(LocationIndicatorType.ORIGINATING_METEOROLOGICAL_WATCH_OFFICE, IWXXMField.ORIGINATING_MWO)
+                .locationIndicator(LocationIndicatorType.ISSUING_AIR_TRAFFIC_SERVICES_UNIT, IWXXMField.ISSUING_ATS_UNIT)
+                .locationIndicator(LocationIndicatorType.ISSUING_AIR_TRAFFIC_SERVICES_REGION, IWXXMField.ISSUING_ATS_REGION)
+                .build();
+        scannersMap.put(new ScannerKey("SIGMET"), sigmetAirmetScanner);
+        scannersMap.put(new ScannerKey("TropicalCycloneSIGMET"), sigmetAirmetScanner);
+        scannersMap.put(new ScannerKey("VolcanicAshSIGMET"), sigmetAirmetScanner);
+        scannersMap.put(new ScannerKey("AIRMET"), sigmetAirmetScanner);
 
-        final GenericTCAIWXXMScanner genericTcaScanner = new GenericTCAIWXXMScanner(tcaFieldProvider);
-        scannersMap.put(new ScannerKey("TropicalCycloneAdvisory"), genericTcaScanner);
+        // Space Weather Advisory scanner
+        final GenericIWXXMScanner swxScanner = GenericIWXXMScanner.builder()
+                .fieldProvider(new SWXFieldXPathProvider())
+                .messageType(MessageType.SPACE_WEATHER_ADVISORY)
+                .requireReportStatus(true)
+                .extractValidityTime(false)
+                .locationIndicator(LocationIndicatorType.ISSUING_CENTRE, IWXXMField.ISSUING_CENTRE)
+                .build();
+        scannersMap.put(new ScannerKey("SpaceWeatherAdvisory"), swxScanner);
 
-        final GenericVAAIWXXMScanner genericVaaScanner = new GenericVAAIWXXMScanner(vaaFieldProvider);
-        scannersMap.put(new ScannerKey("VolcanicAshAdvisory"), genericVaaScanner);
+        // Volcanic Ash Advisory scanner
+        final GenericIWXXMScanner vaaScanner = GenericIWXXMScanner.builder()
+                .fieldProvider(new VAAFieldXPathProvider())
+                .messageType(MessageType.VOLCANIC_ASH_ADVISORY)
+                .requireReportStatus(false)
+                .extractValidityTime(false)
+                .locationIndicator(LocationIndicatorType.ISSUING_CENTRE, IWXXMField.ISSUING_CENTRE)
+                .build();
+        scannersMap.put(new ScannerKey("VolcanicAshAdvisory"), vaaScanner);
 
-        final GenericSIGMETAIRMETIWXXMScanner genericSigmetScanner = new GenericSIGMETAIRMETIWXXMScanner(sigmetAirmetFieldProvider);
-        scannersMap.put(new ScannerKey("SIGMET"), genericSigmetScanner);
-        scannersMap.put(new ScannerKey("TropicalCycloneSIGMET"), genericSigmetScanner);
-        scannersMap.put(new ScannerKey("VolcanicAshSIGMET"), genericSigmetScanner);
-        scannersMap.put(new ScannerKey("AIRMET"), genericSigmetScanner);
-
-        final GenericMETARSPECIIWXXMScanner genericMetarScanner = new GenericMETARSPECIIWXXMScanner(metarFieldProvider);
-        scannersMap.put(new ScannerKey("METAR"), genericMetarScanner);
-        scannersMap.put(new ScannerKey("SPECI"), genericMetarScanner);
+        // Tropical Cyclone Advisory scanner
+        final GenericIWXXMScanner tcaScanner = GenericIWXXMScanner.builder()
+                .fieldProvider(new TCAFieldXPathProvider())
+                .messageType(MessageType.TROPICAL_CYCLONE_ADVISORY)
+                .requireReportStatus(false)
+                .extractValidityTime(false)
+                .locationIndicator(LocationIndicatorType.ISSUING_CENTRE, IWXXMField.ISSUING_CENTRE)
+                .build();
+        scannersMap.put(new ScannerKey("TropicalCycloneAdvisory"), tcaScanner);
 
         return scannersMap;
     }
---
+++ src/main/java/fi/fmi/avi/converter/iwxxm/generic/GenericIWXXMScanner.java
@@ -0,0 +1,212 @@
+package fi.fmi.avi.converter.iwxxm.generic;
+
+import fi.fmi.avi.converter.ConversionIssue;
+import fi.fmi.avi.converter.IssueList;
+import fi.fmi.avi.model.GenericAviationWeatherMessage;
+import fi.fmi.avi.model.MessageType;
+import fi.fmi.avi.model.immutable.GenericAviationWeatherMessageImpl;
+import org.w3c.dom.Element;
+
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpressionException;
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+
+/**
+ * A configurable, version-agnostic generic IWXXM scanner that can be configured
+ * declaratively for any message type.
+ * <p>
+ * Configuration options:
+ * <ul>
+ *   <li>A message type resolver (fixed or dynamic based on element name)</li>
+ *   <li>Whether report status is required or optional</li>
+ *   <li>Whether to extract validity time</li>
+ *   <li>Which location indicators to extract</li>
+ *   <li>A field XPath provider for the message type</li>
+ * </ul>
+ * <p>
+ * Example usage:
+ * <pre>
+ * GenericIWXXMScanner tafScanner = GenericIWXXMScanner.builder()
+ *         .fieldProvider(new TAFFieldXPathProvider())
+ *         .messageType(MessageType.TAF)
+ *         .requireReportStatus(true)
+ *         .extractValidityTime(true)
+ *         .locationIndicator(LocationIndicatorType.AERODROME, IWXXMField.AERODROME)
+ *         .build();
+ * </pre>
+ */
+public class GenericIWXXMScanner extends AbstractGenericAviationWeatherMessageScanner {
+
+    private final Function<Element, MessageType> messageTypeResolver;
+    private final boolean requireReportStatus;
+    private final boolean extractValidityTime;
+    private final Map<GenericAviationWeatherMessage.LocationIndicatorType, IWXXMField> locationIndicatorFields;
+
+    private GenericIWXXMScanner(final Builder builder) {
+        super(builder.fieldXPathProvider);
+        this.messageTypeResolver = Objects.requireNonNull(builder.messageTypeResolver, "messageTypeResolver");
+        this.requireReportStatus = builder.requireReportStatus;
+        this.extractValidityTime = builder.extractValidityTime;
+        this.locationIndicatorFields = Collections.unmodifiableMap(
+                new EnumMap<>(builder.locationIndicatorFields));
+    }
+
+    /**
+     * Creates a new builder for configuring a GenericIWXXMScanner.
+     *
+     * @return a new builder instance
+     */
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    @Override
+    public IssueList collectMessage(final Element featureElement,
+                                    final XPath xpath,
+                                    final GenericAviationWeatherMessageImpl.Builder builder)
+            throws XPathExpressionException {
+        final IssueList issues = new IssueList();
+
+        // 1. Resolve message type
+        final MessageType messageType = messageTypeResolver.apply(featureElement);
+        if (messageType == null) {
+            issues.add(new ConversionIssue(ConversionIssue.Severity.ERROR,
+                    ConversionIssue.Type.SYNTAX,
+                    "Unable to determine message type from element: " + featureElement.getLocalName()));
+            return issues;
+        }
+        builder.setMessageType(messageType);
+
+        // 2. Report status (required or optional based on config)
+        if (requireReportStatus) {
+            collectReportStatus(featureElement, xpath, builder, issues);
+        } else {
+            collectOptionalReportStatus(featureElement, xpath, builder);
+        }
+
+        // 3. Issue time (always extracted)
+        collectIssueTimeUsingFieldProvider(featureElement, xpath, builder, issues);
+
+        // 4. Validity time (if configured)
+        if (extractValidityTime) {
+            collectValidityTimeUsingFieldProvider(featureElement, xpath, builder, issues);
+        }
+
+        // 5. Location indicators (based on config)
+        if (!locationIndicatorFields.isEmpty()) {
+            collectLocationIndicatorsUsingFieldProvider(featureElement, xpath, builder,
+                    locationIndicatorFields, issues);
+        }
+
+        return issues;
+    }
+
+    /**
+     * Builder for creating configured GenericIWXXMScanner instances.
+     */
+    public static class Builder {
+        private final Map<GenericAviationWeatherMessage.LocationIndicatorType, IWXXMField> locationIndicatorFields =
+                new EnumMap<>(GenericAviationWeatherMessage.LocationIndicatorType.class);
+        private FieldXPathProvider fieldXPathProvider;
+        private Function<Element, MessageType> messageTypeResolver;
+        private boolean requireReportStatus = true;
+        private boolean extractValidityTime = false;
+
+        private Builder() {
+        }
+
+        /**
+         * Sets the field XPath provider for this scanner.
+         * This is required.
+         *
+         * @param provider the field XPath provider
+         * @return this builder
+         */
+        public Builder fieldProvider(final FieldXPathProvider provider) {
+            this.fieldXPathProvider = Objects.requireNonNull(provider, "fieldXPathProvider");
+            return this;
+        }
+
+        /**
+         * Sets a fixed message type for this scanner.
+         * Use this when the scanner handles only one message type.
+         *
+         * @param messageType the fixed message type
+         * @return this builder
+         */
+        public Builder messageType(final MessageType messageType) {
+            Objects.requireNonNull(messageType, "messageType");
+            this.messageTypeResolver = element -> messageType;
+            return this;
+        }
+
+        /**
+         * Sets a dynamic message type resolver for this scanner.
+         * Use this when the scanner handles multiple message types (e.g., METAR/SPECI).
+         *
+         * @param resolver function that determines message type from the root element
+         * @return this builder
+         */
+        public Builder messageTypeResolver(final Function<Element, MessageType> resolver) {
+            this.messageTypeResolver = Objects.requireNonNull(resolver, "messageTypeResolver");
+            return this;
+        }
+
+        /**
+         * Sets whether report status is required.
+         * If true (default), an error is added when report status cannot be parsed.
+         * If false, missing report status is silently ignored (for VAA/TCA in IWXXM 2.1).
+         *
+         * @param required whether report status is required
+         * @return this builder
+         */
+        public Builder requireReportStatus(final boolean required) {
+            this.requireReportStatus = required;
+            return this;
+        }
+
+        /**
+         * Sets whether to extract validity time.
+         * Default is false.
+         *
+         * @param extract whether to extract validity time
+         * @return this builder
+         */
+        public Builder extractValidityTime(final boolean extract) {
+            this.extractValidityTime = extract;
+            return this;
+        }
+
+        /**
+         * Adds a location indicator to extract.
+         *
+         * @param type  the location indicator type
+         * @param field the IWXXM field that provides the XPath for this location indicator
+         * @return this builder
+         */
+        public Builder locationIndicator(final GenericAviationWeatherMessage.LocationIndicatorType type,
+                                         final IWXXMField field) {
+            this.locationIndicatorFields.put(
+                    Objects.requireNonNull(type, "type"),
+                    Objects.requireNonNull(field, "field"));
+            return this;
+        }
+
+        /**
+         * Builds the configured GenericIWXXMScanner.
+         *
+         * @return a new GenericIWXXMScanner instance
+         * @throws NullPointerException if required fields are not set
+         */
+        public GenericIWXXMScanner build() {
+            Objects.requireNonNull(fieldXPathProvider, "fieldProvider must be set");
+            Objects.requireNonNull(messageTypeResolver, "messageType or messageTypeResolver must be set");
+            return new GenericIWXXMScanner(this);
+        }
+    }
+}
+
